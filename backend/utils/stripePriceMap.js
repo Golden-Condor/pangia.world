dcb7ce6d01650aa3e82efd84af46c3684317b808
Connor <connor@Connors-MacBook-Pro.local>
2025-05-30 18:45:02 -0400

    Add ObjectId fallback for Starter Pack to Stripe price map

--- backend/utils/stripePriceMap.js
+++ backend/utils/stripePriceMap.js
@@ -6,7 +6,13 @@ const STRIPE_PRICE_MAP = {
   "ceramic-dispenser": "price_1RUb5pDtBv4Vn5bTh29xG6Mj",
   "manual-pump": "price_1RUb8PDtBv4Vn5bToMCjJkmS",
   "brio-tri-temp-cooler": "price_1RUb7uDtBv4Vn5bT38HyaBS0",
-  "6825244a5fff23a69920ca83": "price_1RUauZDtBv4Vn5bTFSZY1Zfa"
+  "6825244a5fff23a69920ca83": "price_1RUauZDtBv4Vn5bTFSZY1Zfa",  // Starter Pack
+  "681fc194fe944aaa58b31e1b": "price_1RUazbDtBv4Vn5bTZLipWJW7", // 20oz (24-Pack)
+  "6825244a5fff23a69920ca7e": "price_1RUb5pDtBv4Vn5bTh29xG6Mj", // Ceramic Dispenser
+  "6825244a5fff23a69920ca80": "price_1RUayrDtBv4Vn5bTSdgnUr80", // Jug Exchange
+  "6825244a5fff23a69920ca81": "price_1RUb8PDtBv4Vn5bToMCjJkmS", // Manual Pump
+  "6825244a5fff23a69920ca82": "price_1RUaxBDtBv4Vn5bThOoamSJ6", // Single 5 Gallon
+  "6825244a5fff23a69920ca7f": "price_1RUb7uDtBv4Vn5bT38HyaBS0" // Brio Cooler
 };
 
 module.exports = STRIPE_PRICE_MAP;
